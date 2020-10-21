a817ad69f6ffa62a4504528cf9f77210f6b798ff
vitoref <vitorefinco@hotmail.com>
2020-10-21 16:08:13 -0300

    DepartmentDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
---
+++ src/application/Program2.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		Department dep = departmentDao.findById(1);
+		System.out.println(dep);
+		
+	}
+
+}

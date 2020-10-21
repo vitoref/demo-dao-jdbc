2e6cf73aac6f3c68b7a7409030e242594ed37a1e
vitoref <vitorefinco@hotmail.com>
2020-10-21 16:16:44 -0300

    findById e findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -8,10 +10,16 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("=== TEST 1: department findById ===");
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		Department dep = departmentDao.findById(1);
 		System.out.println(dep);
 		
+		System.out.println("=== TEST 2: department findAll ===");
+		List<Department> list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
